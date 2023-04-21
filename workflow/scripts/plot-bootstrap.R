e40b41d6d9624d83f07d59b4d83cf70e9970bd6e
hehestevenhe <115518768+hehestevenhe@users.noreply.github.com>
2023-04-21 18:00:41 +1000

    Overhaul script to support TRAWLING entries
    A large number of changes were made to the code to support TRAWLING entries
    - Now containes two instances for generating plots; first to visualise the top n transcripts, followed by a second instance that then visualises transcripts for genes of interest
    
    - The original code combined the top n genes with genes of interest into a single list to generate the plots in one go. The two main issues in the way it was coded were 1) only a single transcript was visualised for each gene of interest in the param file and 2) only canonical transcripts were being visualised. These have been addressed
    
    - Both canonical and non-canonical transcripts within the top n are visualised, with all transcripts for a gene of interest also now visualised provided there are no NA values in the data (excepting canonical status), regardless of significance
    
    - Replaces ":" with "_" from transcript names when saving, which caused compatibility issues (files cannot be saved with ":" special characters)
    
    
    Of note, in the original code a list of genes (including genes of interest) is created, with an additional entry created for "Custom" transcript <add_row(ext_gene = "Custom") >
    This is redundant though, as in the sleuth_init rule, the R script automatically assigns custom transcripts with NA values for ens_gene and canonical columns resulting in them getting dropped due to a <drop_na()> command. While this commit removes this code, functionality for custom transcripts still remains missing. This comment here highlights this in case we wish to fix this functionality in the future.

--- workflow/scripts/plot-bootstrap.R
+++ workflow/scripts/plot-bootstrap.R
@@ -11,38 +11,40 @@ top_n <- -strtoi(snakemake@params["top_n"])
 
 results <- read_tsv(snakemake@input[["transcripts"]])
 
-top_genes <- results %>%
-    filter(qval <= snakemake@params[["fdr"]]) %>%
-    top_n(top_n, qval) %>%
-    dplyr::select(ext_gene) %>%
-    drop_na() %>%
-    distinct(ext_gene)
+dir.create( snakemake@output[[1]] )
 
+# Create plots for top n transcripts
+top_transcripts <- results %>%    
+    filter(qval <= snakemake@params[["fdr"]]) %>%        
+    top_n(top_n,qval) %>%
+    select(., c("ext_gene","target_id"))
+    drop_na()
 
+for (i in nrow(top_transcripts)) {
+    plot_bootstrap(so, top_transcripts[i,"target_id"], color_by = snakemake@params[["color_by"]], units = "tpm")
+    transcript_save <- str_replace_all(top_transcript[i, "target_id"], ":", "_")
+    ggsave(file = str_c(snakemake@output[[1]], "/", top_transcript[i,"ext_gene"], ".", transcript_save, ".", snakemake@wildcards[["model"]] , ".bootstrap.pdf"))
+    }
+
+# Create plots for genes of interest
 if ( !is.null(snakemake@params[["genes"]]) ) {
-  genes_of_interest <- tibble( ext_gene = snakemake@params[["genes"]]) %>%
+  genes <- tibble( ext_gene = snakemake@params[["genes"]]) %>%
     distinct(ext_gene)
 } else {
   # "genes" is null, if the list provided in config.yaml is empty
-  genes_of_interest <- tibble( ext_gene = character() )
+  genes <- tibble( ext_gene = character() )
 }
 
-genes <- full_join(top_genes, genes_of_interest, by = 'ext_gene') %>%
-    add_row(ext_gene = "Custom") %>%
-    pull(ext_gene)
-
-dir.create( snakemake@output[[1]] )
-
-for (gene in genes) {
-    transcripts <- results %>%
-        filter(ext_gene == gene) %>%
+for (gene in genes){
+    transcripts <- results[,!(names(results) %in% "canonical")]  %>%   # Removes the canonical column, non-canonical transcripts are
+        filter(ext_gene == gene) %>%                                   # otherwise not visualised due to the drop_na()
         drop_na() %>%
         pull(target_id)
 
     if ( length( transcripts > 0 ) ) {
         for (transcript in transcripts) {
             plot_bootstrap(so, transcript, color_by = snakemake@params[["color_by"]], units = "tpm")
-            ggsave(file = str_c(snakemake@output[[1]], "/", gene, ".", transcript, ".", snakemake@wildcards[["model"]] , ".bootstrap.pdf"))
+            ggsave(file = str_c(snakemake@output[[1]], "/", gene, ".", str_replace_all(transcript, ":", "_"), ".", snakemake@wildcards[["model"]] , ".bootstrap.pdf"))
         }
     }
 }
