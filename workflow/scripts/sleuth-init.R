3c8b0dc1cd3ae72a3b4e4922799053701e7acfcb
hehestevenhe <115518768+hehestevenhe@users.noreply.github.com>
2023-04-21 15:13:47 +1000

    Adds TRAWLING annotation matching
    Updates custom transcripts from TRAWLING such that the code searches fro the corresponding transcript id, and copies the annotation to the custom entry (gene name, gene id, gene description). These are then saved to the rds object so that these custom transcripts don't simply have NA values, which result in them not being visualised later in the pipeline

--- workflow/scripts/sleuth-init.R
+++ workflow/scripts/sleuth-init.R
@@ -78,20 +78,22 @@ if(!length(custom_transcripts) == 0) {
     so$target_mapping <- so$target_mapping %>%
                         # add those custom transcripts as rows to the target mapping
                         add_row(ens_gene = NA, ext_gene = "Custom", target_id = custom_transcripts, canonical = NA)
-    #Extra code below; EDIT SO THAT PARAMS TO DETERMINE CUSTOM suffix
-    invisible(lapply(1:nrow(data),
-    function(i){
-          if (str_detect(data[i,"target_id"], 'premrna') == TRUE)
-     		{
-       		try({  
-         	transcript_id <<- str_extract(data[i,"target_id"], ".*(?=:premrna)")
-                matched_trans_row <<- which(data$target_id == transcript_id, arr.ind = TRUE)
-                data[i,c("ens_gene", "ext_gene", "gene_desc")] <<- 
-           		data[matched_trans_row,c("ens_gene", "ext_gene", "gene_desc")]},
-       		outFile = print(paste0("error-logs/Errorlog ",i,"_",transcript_id,".txt")))      
-     		}
-  	       }
-	     ))
+
+### Extra code below for TRAWLING support; added params so any custom suffix can be specified
+  invisible(lapply(1:nrow(so$target_mapping),
+     function(i){
+       if (str_detect(so$target_mapping[i,"target_id"], snakemake@params[["suffix"]]) == TRUE)
+       {
+         try({  
+           transcript_id <<- str_extract(so$target_mapping[i,"target_id"], paste0(".*(?=", snakemake@params[["suffix"]],")"))
+           matched_trans_row <<- which(so$target_mapping$target_id == transcript_id, arr.ind = TRUE)
+           so$target_mapping[i,c("ens_gene", "ext_gene", "gene_desc")] <<- 
+             so$target_mapping[matched_trans_row,c("ens_gene", "ext_gene", "gene_desc")] } ,
+           outFile = print(paste0("logs/sleuth-init-error-log ",i,"_",transcript_id,".txt")))      
+       }
+     }
+  ))
+### Extra code ends here
 }
 
 if(!is.null(model[["full"]])) {
